5794d8067d33b31e02dd72a3a55d5e588d808bba
jimin <jimin@DESKTOP-P8OHN7D>
2019-11-28 22:41:09 +0900

    Calpedar,Prompt update

---
+++ src/jimin/calendar/Prompt.java
@@ -0,0 +1,38 @@
+package jimin.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal>";
+
+	public void runPrompt() {
+		Scanner scan = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		int month = 1;
+
+		while (true) {
+			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
+			month = scan.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if (month > 12) {
+				continue;
+			}
+			cal.printCalendar(2017, month);
+		}
+
+		System.out.println("bye~");
+		scan.close();
+	}
+
+	public static void main(String[] args) {
+
+		Prompt p = new Prompt();
+		p.runPrompt();
+
+	}
+}
