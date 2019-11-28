abff987dbf4cb4b58228dbb18a5cb073878762f7
jimin <jimin@DESKTOP-P8OHN7D>
2019-11-28 22:15:51 +0900

    update for

--- src/jimin/calendar/Calendar.java
+++ src/jimin/calendar/Calendar.java
@@ -15,7 +15,8 @@ public class Calendar {
 		/*
 		 * public static void printSampleCalendar(){
 		 * System.out.println("일  월  화  수  목  금  토");
-		 * System.out.println("--------------------");
+		 * System.out.println("----
+		 * ----------------");
 		 * System.out.println("1  2  3  4  5  6  7");
 		 * System.out.println("8  9  10 11 12 13 14");
 		 * System.out.println("15 16 17 18 19 20 21");
@@ -26,13 +27,17 @@ public class Calendar {
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요");
-		int month = scan.nextInt();
 
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month - 1));
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scan.nextInt();
 
-		cal.printSampleCalendar();
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("달을 입력하세요");
+			int month = scan.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month - 1));
+		}
 
+		System.out.println("bye~");
 		scan.close();
 
 	}
